513f0a1eac851a532d1a30a1eb88308f0e726fe8
Mariusz Jakubiak <s9725mj@ms.wwsi.edu.pl>
2022-11-05 12:57:47 +0100

    edit .gitignore file

---
+++ FilmDB/FilmDB/Models/FilmModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB.Models
+{
+    public class FilmModel
+    {
+        public int ID { get; set; }
+        public int Year { get; set; }
+        public string Title { get; set; }
+        
+    }
+
+
+}
